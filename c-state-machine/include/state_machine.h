a88ed352eda0313cae1144360e73e4916feac56f
tgollick <thomasgollick@gmail.com>
2026-05-05 21:30:12 +0100

    Defining states, commands and an inital object for the StateMachine.

--- c-state-machine/include/state_machine.h
+++ c-state-machine/include/state_machine.h
@@ -1,1 +1,32 @@
-﻿/* state_machine.h â€
+﻿// This file outlines the states, commands and a struct to hold a basic state machine object.
+
+// All states outlined in the state-diagram.md file
+typedef enum {
+    STATE_SAFE,
+    STATE_ARMING,
+    STATE_ARMED,
+    STATE_ENABLED,
+    STATE_FIRED,
+    STATE_FAULT,
+    STATE_DISARMING,
+    STATE_COUNT,
+} State;
+
+// All commands outlined in the state-diagram.md file
+typedef enum {
+    CMD_ARM,
+    CMD_ABORT,
+    CMD_ENABLE,
+    CMD_FIRE,
+    CMD_RESET,
+    CMD_ARMING_COMPLETE,
+    CMD_DISARMING_COMPLETE,
+    CMD_FAULT_DETECTED,
+    CMD_COUNT,
+} Command;
+
+typedef struct {
+    State current_state;
+
+    // Will outline more members of this object later down the line
+} StateMachine;
